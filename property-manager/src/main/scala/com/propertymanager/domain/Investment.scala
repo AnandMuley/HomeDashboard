585509046b765d53fe98a0cc7e2ab420e347edd9
AnandMuley <muley.anand66@gmail.com>
2017-11-13 23:45:17 +0530

    [AM] Trying out var and val

--- property-manager/src/main/scala/com/propertymanager/domain/Investment.scala
+++ property-manager/src/main/scala/com/propertymanager/domain/Investment.scala
@@ -4,10 +4,10 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "investments")
-class Investment(name: String, amount: Double) {
+class Investment(var itsType: String,var amount: Double) {
 
   @Id
   private val id: String = null
 
-
+  override def toString = s"Investment($id, $itsType, $amount)"
 }
--- property-manager/src/main/scala/com/propertymanager/runner/SampleRunner.scala
+++ property-manager/src/main/scala/com/propertymanager/runner/SampleRunner.scala
@@ -1,12 +1,12 @@
 package com.propertymanager.runner
 
-import com.propertymanager.model.Payment
+import com.propertymanager.domain.Investment
 
 object SampleRunner {
 
   def main(args: Array[String]): Unit = {
-    val payment = new Payment("Online")
-    println(s" ID : ${payment.getId()}")
+    val investment = new Investment("Ronnie", 1200)
+    println(s" ID : ${investment.amount}")
   }
 
 }
